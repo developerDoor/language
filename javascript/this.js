6f09fc0b13f2a7432d40eec7fdd6226c33c15c88
jhmoon <dothebestmoon1016@gmail.com>
2024-01-12 17:37:33 +0900

    this temp2

--- javascript/this.js
+++ javascript/this.js
@@ -33,4 +33,52 @@ function Person(name, year) {
 
 const yuJin2 = new Person('안유진', 2003)
 console.log(yuJin2.sayHello());
-P
+
+
+// 객체의 method로 가장 상위 레벨에 함수를 선언하면
+// this가 자동으로 실행하는 대상 객체로 매핑이 된다.
+// 하지만 그외 위치에 함수를 선언하면 함수의 this는 무조건 global 객체에 매핑된다.
+Person.prototype.dance = function () {
+	function dance2 () {
+		return `${this.name}이 춤을 춥니다.`
+	}
+
+	return dance2();
+
+}
+console.log(yuJin2.dance());
+
+/**
+ *	this 키워드가 어떤 걸 가리키냐는 세가지만 기억하자.
+ *  1) 일반 함수를 호출할 땐 this가 최상위 객체(global, window)를 가리킨다.
+ *  2) 메서드로 호출할 땐 호출된 객체를 가리킨다.
+ *  3) new 키위드를 사용해서 객체를 생성했을때는 객체를 가리킨다.
+ */
+
+/**
+ * 1) apply() -> 인자를 리스트로 입력해야한다.
+ * 2) call() -> 콤마를 기반으로 인자를 순서대로 넘겨줘야한다.
+ * 3) bind() -> this를 바인딩만 해놓고 나중에 실행할 수 있다.
+ */
+function returnName() {
+	return this.name;
+}
+
+console.log(returnName());
+
+const yuJin3 = {
+	name: '안유진'
+}
+
+console.log(returnName.call(yuJin3));
+
+function multiply(x, y, z) {
+	return `${this.name} / 결과값 : ${x * y * z}`;
+}
+
+console.log(multiply.call(yuJin3, 3, 4, 5));
+console.log(multiply.apply(yuJin3, [3, 4, 5]));
+
+const laterFunc = multiply.bind(yuJin3, 3,4,5)
+console.log(laterFunc);
+console.log(laterFunc())
